bcb6a0b58665398a481d9d986194ebc7a2aea9fc
tommiecas <157588974+tommiecas@users.noreply.github.com>
2024-07-29 10:50:08 -0700

    Player Character and Enemy Part Two

--- Source/Aura/Private/Characters/AuraCharacterBase.cpp
+++ Source/Aura/Private/Characters/AuraCharacterBase.cpp
@@ -1,12 +1,14 @@
 // Copyright Ryter Productions
 
 
-#include "AuraCharacterBase.h"
+#include "Characters/AuraCharacterBase.h"
 
 AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
-
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(),FName("Weapon Socket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
