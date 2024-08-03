00a9b52c675ef0e5d3ca632b834ebc8d03b9bcf7
tommiecas <157588974+tommiecas@users.noreply.github.com>
2024-08-02 17:42:57 -0700

    Infinite Gameplay Effects

--- Source/Aura/Private/Actors/AuraEffectActor.cpp
+++ Source/Aura/Private/Actors/AuraEffectActor.cpp
@@ -37,5 +37,13 @@ void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGam
 	TargetAsc->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
 }
 
+void AAuraEffectActor::OnOverlap(AActor* TargetActor)
+{
+}
+
+void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
+{
+}
+
 
 
